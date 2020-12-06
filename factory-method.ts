c18ba3df634e1490807dfd9803997de783a71358
Sergey Makhonya <admin@example.com>
2020-12-06 20:50:21 +0800

    factory-method

---
+++ factory-method.ts
@@ -0,0 +1,43 @@
+interface IAnimal {
+    say(): void;
+}
+
+class Cat implements IAnimal {
+    say() {
+        console.log('Мяяяяууу!!!');
+    }
+}
+
+class Dog implements IAnimal {
+    say() {
+        console.log('Гааааввв!!!');
+    }
+}
+
+//-----
+
+interface AnimalCreator {
+    createAnimal(): IAnimal;
+}
+
+class CatCreator implements AnimalCreator {
+    createAnimal() {
+        return new Cat();
+    }
+}
+
+class DogCreator implements AnimalCreator {
+    createAnimal() {
+        return new Dog();
+    }
+}
+
+//-----
+
+const catCreator = new CatCreator();
+const dogCreator = new DogCreator();
+const cat = catCreator.createAnimal();
+const dog = dogCreator.createAnimal();
+
+cat.say();
+dog.say();
